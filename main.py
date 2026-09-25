6da6a030e903c463640e6f765d165b3bf39901f3
halfnoided <halfnoided@gmail.com>
2026-09-25 17:25:13 +0300

    Add database integration to store parsed feeds.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from rssparser import rss_parser
-from database import engine, Base
+from rssparser import rss_parser, save_feed_to_db
+from database import engine, Base, SessionLocal
 from models import Feed, Article
 
 Base.metadata.create_all(bind=engine)
@@ -13,15 +13,8 @@ Base.metadata.create_all(bind=engine)
 url = "https://habr.com/ru/rss/articles/"
 
 def main():
-    # s1 = path
-    s2 = url
-    parsed_s2 = rss_parser(s2)
-    # Accessing first 5 entries with their title
-    for entry in parsed_s2.entries[:5]:
-        print(entry.get("title", "No specific title")) 
-        print(entry.get("link"))
-        print(entry.get("published"))
-        print("That's it!")
+    with SessionLocal() as session:
+        save_feed_to_db(rss_parser(url), session)
     return 0
 
 if __name__ == "__main__":
--- rssparser.py
+++ rssparser.py
@@ -1,5 +1,37 @@
-from feedparser import parse 
+from feedparser import parse
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from models import Feed, Article 
 
 def rss_parser(source: str):
-    parsed_result = parse(source)
-    return parsed_result
+    parsed_source = parse(source)
+    return parsed_source
+
+def save_feed_to_db(parsed_source, session):
+    feed_url = parsed_source.feed.get("link", "unknown_url")
+    feed_title = parsed_source.feed.get("title", "Unknown title")
+    
+    feed = session.execute(
+        select(Feed)
+        .where(Feed.url == feed_url)
+        ).scalars().one_or_none()
+
+    if not feed:
+        feed = Feed(
+            url = feed_url,
+            title = feed_title
+        )
+        session.add(feed)
+        session.commit()
+    
+    for entry in parsed_source.entries:
+        article = Article(
+            feed_id = feed.id,
+            link = entry.get("link"),
+            title = entry.get("title")
+        )
+        try:
+            session.add(article)
+            session.commit()
+        except IntegrityError:
+            session.rollback()
